597b3d66ecf8893a3e17e655d64322b392f0c10e
Joel Nitta <joelnitta@gmail.com>
2023-03-02 16:44:19 +0900

    Update slides

--- print.R
+++ print.R
@@ -3,4 +3,4 @@ library(pagedown)
 # print slides
 chrome_print(
   "https://joelnitta.github.io/jsps-2023-03-03",
-  output = "slides.pdf", format = "pdf")
+  output = "working/slides.pdf", format = "pdf")
